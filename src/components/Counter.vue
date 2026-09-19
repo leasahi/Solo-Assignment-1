7dda1559bfe827d9f004d289e6bd96ad2cb3021b
Lea Sahiner <leasahiner@Mac.fritz.box>
2026-09-20 01:20:28 +0200

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {computed, ref} from 'vue'
+
+const count = ref(0)
+const triple = computed(() => count.value*3)
+</script>
+
+<template>
+  <h1>
+    Count: {{ count }}
+  </h1>
+  <h2>
+    Triple of Count: {{ triple }}
+  </h2>
+  <div class="buttons">
+    <button type="button" @click="count--">
+      <h2>
+        -
+      </h2>
+    </button>
+    <button type="button" @click="count++">
+      <h2>
+        +
+      </h2>
+    </button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
